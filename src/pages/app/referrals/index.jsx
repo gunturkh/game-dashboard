e6ee13baa3fd981f998d83b3f2763ecd4f5b12cd
Guntur KH <guntur.kh@gmail.com>
2024-08-09 17:42:23 +0700

    feat: referrals

--- src/pages/app/referrals/index.jsx
+++ src/pages/app/referrals/index.jsx
@@ -5,7 +5,57 @@ import { useNavigate } from "react-router-dom";
 import useWidth from "@/hooks/useWidth";
 import { useDispatch } from "react-redux";
 import LevelDetailTable from "@/components/partials/Table/level-detail-table";
+import { Tree } from "react-arborist";
 import Card from "@/components/ui/Card";
+
+export const data = [
+  { id: "1", name: "Unread" },
+  { id: "2", name: "Threads" },
+  {
+    id: "3",
+    name: "Chat Rooms",
+    children: [
+      {
+        id: "c1",
+        name: "General",
+        children: [
+          { id: "cg1", name: "Random" },
+          { id: "cg2", name: "Open Source Projects" },
+        ],
+      },
+      { id: "c2", name: "Random" },
+      { id: "c3", name: "Open Source Projects" },
+    ],
+  },
+  {
+    id: "4",
+    name: "Direct Messages",
+    children: [
+      { id: "d1", name: "Alice" },
+      { id: "d2", name: "Bob" },
+      { id: "d3", name: "Charlie" },
+    ],
+  },
+];
+
+function Node({ node, style, dragHandle }) {
+  /* This node instance can do many things. See the API reference. */
+  return (
+    <div
+      style={style}
+      ref={dragHandle}
+      onClick={() => {
+        console.log("node", node);
+        node.toggle();
+      }}
+    >
+      {node.children.length > 0 ? "👥" : "👤"}{" "}
+      {`${node.data.username} ${
+        node.children.length > 0 ? `: (${node.children.length})` : ""
+      } `}
+    </div>
+  );
+}
 
 function Referrals() {
   const navigate = useNavigate();
@@ -39,6 +89,23 @@ function Referrals() {
       <div className="flex flex-wrap justify-between items-center mb-4"></div>
 
       <Card title={"Referrals"} noborder>
+        <Tree
+          initialData={getReferrals}
+          openByDefault={false}
+          /* An accessor can provide a string property name */
+          idAccessor="username"
+          /* or a function with the data as the argument */
+          childrenAccessor={(d) => d.referral}
+          // width={600}
+          // height={1000}
+          indent={24}
+          rowHeight={36}
+          paddingTop={10}
+          paddingBottom={10}
+          padding={25 /* sets both */}
+        >
+          {Node}
+        </Tree>
         {/* <LevelDetailTable levelsData={getLevels} /> */}
       </Card>
     </div>
